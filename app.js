e71b43857f48dba376f9e155513fcf0f4d5d7d2b
muhammadhassam2022skipq <muhammad.hassam.skipq@gmail.com>
2022-07-19 22:01:54 +0500

    Completed Assignment

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
+// creating express app and importing required modules
 const express = require("express");
 const app = express();
-const fs = require ("fs");
-const path = require('path');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+//selecting the port
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// creating express app
+
+//Express sever condition
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html")); //serving static Html file
+});
+
+app.post("/message", (req, res) => {
+  const strSalaryValue = req.body.salaray;
+  var currentSalary = parseInt(strSalaryValue);
+  console.log(currentSalary);
+  fs.writeFileSync("message.txt", strSalaryValue); //sending our from data to text file (optional)
+  const stringSalary = fs.readFileSync("message.txt", "utf8"); // reading from text file (optional)
+  
+  //conditional statements depend upon our problem statement
+  if (currentSalary < 1e5) {
+    var salaryTax = 0;
+    var salaryPercentage= 0;
+  } else if (currentSalary >= 1e5 && currentSalary < 2e5) {
+    salaryTax = 0.1 * currentSalary;
+    salaryPercentage= 0.1 * 100;
+  } else if (currentSalary >= 2e5 && currentSalary < 3e5) {
+    salaryTax = 0.15 * currentSalary;
+    salaryPercentage= 0.15 * 100;
+  } else if (currentSalary >= 3e5 && currentSalary < 4e5) {
+    salaryTax = 0.2 * currentSalary;
+    salaryPercentage= 0.2 * 100;
+  } else {
+    salaryTax = 0.25 * currentSalary;
+    salaryPercentage= 0.25 * 100;
+  }
+
+  //sending the response
+  res.send("The tax on the person with salary " + currentSalary + " is " + salaryTax+ " Rs which is "+salaryPercentage+"% of the income");
+});
 
 
-//condition for home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
-
-app.post("/message", (req,res,next)=> {
-   
-    // console.log("post message is: "+m);
-    fs.writeFileSync('message.txt', req.body.salaray);
-    res.send("<h1> your response has been submitted </h1>")
-    // res.redirect("/");
-})
-
+//listening the port
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
